cd16fd4375a0ca95bae808303955ee050e4c2745
nitha-nimal <nitha.nimal+github@proton.me>
2026-04-23 09:29:28 -0600

    Updated electronics_sales_query.sql file

--- electronics_sales_query.sql
+++ electronics_sales_query.sql
@@ -1,8 +1,12 @@
+-- =========================================
+-- 1. DATA CLEANING
+-- =========================================
+
 -- Check for NULL values
 SELECT *
 FROM `trim-glazing-478812-b5.salesdata.officesupplies`
 WHERE `Order Date` IS NULL 
-  OR `Product Name` = 'NULL'
+  OR `Product Name` IS NULL
   OR Category = 'NULL'
   OR Region = 'NULL'
   OR Quantity = 0
@@ -10,32 +14,40 @@ WHERE `Order Date` IS NULL
   OR Profit = 0;
 
 
--- Total Revenue
-SELECT
+-- =========================================
+-- 2. KPI METRICS
+-- =========================================
+
+-- Overall KPIs
+SELECT 
+  SUM(Sales) AS total_revenue,
+  ROUND(SUM(Profit),2) AS total_profit,
+  SUM(Quantity) AS total_units_sold,
+  ROUND(SAFE_DIVIDE(SUM(Profit), SUM(Sales)) * 100,3) AS profit_margin,
+  ROUND(SAFE_DIVIDE(SUM(Sales), SUM(Quantity)),2) AS avg_price_per_item
+FROM `trim-glazing-478812-b5.salesdata.officesupplies`;
+
+
+-- =========================================
+-- 3. BUSINESS INSIGHTS
+-- =========================================
+
+-- Sales by Region
+SELECT Region,
   SUM(Sales) AS total_revenue
-FROM `trim-glazing-478812-b5.salesdata.officesupplies`;
-
-
--- Total Profit
-SELECT
-  ROUND(SUM(Profit),2) AS total_profit
-FROM `trim-glazing-478812-b5.salesdata.officesupplies`;
-
-
--- Total Units Sold
-SELECT
-  SUM(Quantity) AS total_units_sold
-FROM `trim-glazing-478812-b5.salesdata.officesupplies`;
-
-
-
--- High Profit Products
-SELECT DISTINCT(`Product Name`) AS Product_Name,
-  Category,
-  ROUND(SUM(Profit), 2) AS sum_profit
 FROM `trim-glazing-478812-b5.salesdata.officesupplies`
-GROUP BY Product_Name, Category
-ORDER BY sum_profit DESC;
+GROUP BY Region
+ORDER BY total_revenue DESC; 
+
+
+-- Category Performance
+SELECT
+  Category,
+  Sum(Sales) AS total_revenue,
+  ROUND(Sum(Profit),2) AS total_profit
+FROM `trim-glazing-478812-b5.salesdata.officesupplies`
+GROUP BY Category
+ORDER BY total_profit DESC; 
 
 
 -- Top Selling Products (High revenue product)
@@ -45,6 +57,15 @@ SELECT DISTINCT(`Product Name`) AS Product_Name,
 FROM `trim-glazing-478812-b5.salesdata.officesupplies`
 GROUP BY Product_Name, Category
 ORDER BY total_sales DESC;
+
+
+-- Most Profitable Products
+SELECT DISTINCT(`Product Name`) AS Product_Name,
+  Category,
+  ROUND(SUM(Profit), 2) AS sum_profit
+FROM `trim-glazing-478812-b5.salesdata.officesupplies`
+GROUP BY Product_Name, Category
+ORDER BY sum_profit DESC;
 
 
 -- Top Product in each region
@@ -60,7 +81,11 @@ FROM(
 WHERE ranking = 1;
 
 
--- Total Revenue over the years
+-- =========================================
+-- 4. TREND ANALYSIS
+-- =========================================
+
+-- Yearly Sales Trend
 SELECT
   Extract(Year from `Order Date`) AS Year,
   SUM(Sales) AS total_revenue,
@@ -69,13 +94,22 @@ GROUP BY Year
 ORDER BY total_revenue DESC;
 
 
+-- Monthly trend
+SELECT
+  EXTRACT(MONTH FROM `Order Date`) AS month_num,
+  FORMAT_DATE('%b', `Order Date`) AS Month,
+  SUM(Sales) AS total_revenue
+FROM `trim-glazing-478812-b5.salesdata.officesupplies`
+GROUP BY month_num, month
+ORDER BY month_num;
+
+
 -- Year over year Revenue growth
 SELECT 
   Year, 
   total_revenue, 
   LAG(total_revenue) OVER (ORDER BY Year) AS pre_revenue,
   SAFE_DIVIDE(total_revenue - LAG(total_revenue) OVER (ORDER BY Year), LAG (total_revenue) OVER (ORDER BY Year)) * 100 AS percent_growth
-
 FROM
   (SELECT
     Extract(Year from `Order Date`) AS Year,
@@ -84,7 +118,11 @@ FROM
   GROUP BY Year); 
 
 
--- Pivot table with year, category, total_revenue to understand the category performance over the years
+-- =========================================
+-- 5. ADVANCED ANALYSIS
+-- =========================================
+
+-- Pivot: Category Sales by Year
 SELECT *
 FROM
 (SELECT
@@ -99,7 +137,7 @@ PIVOT (
 ORDER BY Year;
 
 
--- Total Revenue for different categories in each region
+-- Pivot: Category Sales by Region
 SELECT *
 FROM(
   SELECT
@@ -114,46 +152,6 @@ PIVOT(
   For Category IN ('Electronics', 'Accessories', 'Office')
 )
 ORDER BY Region;
-
-
-
--- Profit Margin (%)
-SELECT
-  ROUND(SAFE_DIVIDE(SUM(Profit), SUM(Sales)) * 100,3) AS profit_margin
-FROM `trim-glazing-478812-b5.salesdata.officesupplies`;
-
-
--- Monthly trend
-SELECT
-  FORMAT_DATE('%b', `Order Date`) AS Month,
-  SUM(Sales) AS total_revenue
-FROM `trim-glazing-478812-b5.salesdata.officesupplies`
-GROUP BY Month
-ORDER BY total_revenue DESC;
-
-
--- Sales by Region
-SELECT Region,
-  SUM(Sales) AS total_revenue
-FROM `trim-glazing-478812-b5.salesdata.officesupplies`
-GROUP BY Region
-ORDER BY total_revenue DESC; 
-
--- Category Performance
-SELECT
-  Category,
-  Sum(Sales) AS total_revenue,
-  ROUND(Sum(Profit),2) AS total_profit
-FROM `trim-glazing-478812-b5.salesdata.officesupplies`
-GROUP BY Category
-ORDER BY total_profit DESC; 
-
-
--- Average Price per Item
-SELECT 
-  ROUND(SAFE_DIVIDE(SUM(Sales), SUM(Quantity)),2) AS avg_price_per_item
-FROM `trim-glazing-478812-b5.salesdata.officesupplies`;
-
 
 
 -- Profitability by Product
